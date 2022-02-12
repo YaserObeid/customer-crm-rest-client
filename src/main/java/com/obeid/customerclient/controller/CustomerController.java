78a09fb6509d061e8132529747e51e5c51e8e2c4
YaserObeid <yaser.obeid@hotmail.com>
2022-02-12 17:03:30 +0100

    complete customer controller

--- src/main/java/com/obeid/customerclient/controller/CustomerController.java
+++ src/main/java/com/obeid/customerclient/controller/CustomerController.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.obeid.customerclient.model.Customer;
 import com.obeid.customerclient.service.CustomerService;
@@ -28,6 +31,57 @@ public class CustomerController {
 		model.addAttribute("customers",customers);
 		return "customer-list";
 	}
+	
+	// open empty form (customer)
+	
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model model) {
+		
+		Customer customer = new Customer();
+		
+		model.addAttribute("customer", customer);
+		
+		return "customer-form";
+	}
+	
+	// add new customer
+	
+	@PostMapping("/saveCustomer")
+	public String saveCustomer(
+			@ModelAttribute("customer")
+			Customer customer) {
+		
+		customerService.saveCustomer(customer);
+		
+		return "redirect:/customer/list";
+	}
+	
+	// open filled form (customer/given_id)
+	
+		@GetMapping("/showFormForUpdate") 
+		public String showFormForUpdate(
+				@RequestParam("customerId") 
+				int id,
+				Model model) {
+			// get customer for the given id
+			Customer customer = customerService.getCustomer(id);
+			
+			// add customer to model
+			model.addAttribute("customer", customer);
+			
+			return "customer-form";
+		}
+		
+		// delete customer by id
+		
+		@GetMapping("/delete")
+		public String deleteCustomer(
+				@RequestParam("customerId") 
+				int id) {
+			customerService.deleteCustomer(id);
+			return "redirect:/customer/list";
+			
+		}
 	
 	
 	
